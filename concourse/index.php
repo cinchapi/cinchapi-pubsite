5b1d12f17ab10a7ca27a5a239db430f03a209e95
Jeff Nelson <jtnelson1@gmail.com>
2013-11-20 15:05:58 -0500

    draft
    Change-Id: Ib509cb9981d982b418d8c8095def088f9c067eb6

---
+++ concourse/index.php
@@ -0,0 +1,181 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+
+	<title>Concourse</title>
+
+	<!-- Bootstrap core CSS -->
+	<link href="../css/bootstrap.css" rel="stylesheet">
+
+	<!-- Custom styles for this template -->
+	<link href="../css/sticky-footer-navbar.css" rel="stylesheet">
+	<style>
+
+    /* GLOBAL STYLES
+    -------------------------------------------------- */
+    /* Padding below the footer and lighter body text */
+
+    .carousel {
+	margin-bottom: 0px;
+	background: #31363a;
+    }
+
+    .carousel .container {
+	margin-left: -2em;
+	position: relative;
+	z-index: 9;
+    }
+
+    .carousel .item {
+      height: 700px;
+    }
+
+
+    .one {
+      background: url(img/slide1.jpg) no-repeat;
+      background-size: cover;
+    }
+
+
+    .carousel-caption {
+      background-color: transparent;
+      position: static;
+      max-width: 900px;
+      margin-top: 35%;
+    }
+    </style>
+
+	<!-- Just for debugging purposes. Don't actually copy this line! -->
+	<!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	  <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+	<![endif]-->
+    </head>
+
+    <body>
+
+	<!-- Wrap all page content here -->
+	<div id="wrap">
+
+	    <!-- Fixed navbar -->
+	    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+		<div class="container">
+		    <div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+			    <span class="sr-only">Toggle navigation</span>
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#"><span class="glyphicon glyphicon-road"></span> Concourse</a>
+		    </div>
+		    <div class="collapse navbar-collapse">
+			<ul class="nav navbar-nav">
+			    <li><a href="#about">Features</a></li>
+			    <li><a href="#about">Getting Started</a></li>
+			    <li><a href="#contact">Documentation</a></li>
+			    <li><a href="#contact">Report Issues</a></li>
+			    <li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Download <b class="caret"></b></a>
+				<ul class="dropdown-menu">
+				    <li class="dropdown-header">Version 0.2 (RC1)</li>
+				    <li><a href="#">concourse-api</a></li>
+				    <li><a href="#">concourse-server</a></li>
+				    <li class="divider"></li>
+				    <li class="dropdown-header">Snapshots</li>
+				    <li><a href="#">concourse-api</a></li>
+				    <li><a href="#">concourse-server</a></li>
+				</ul>
+			    </li>
+			</ul>
+		    </div><!--/.nav-collapse -->
+		</div>
+	    </div>
+	    
+	    <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide">
+      <div class="carousel-inner">
+        <div class="item one active">
+          <div class="container">
+            <div class="carousel-caption jumbotron">
+              <h1 style="">Introducing <span class="text-primary">Concourse</span></h1>
+	      <p>Connect to data.</p>
+            </div>
+          </div>
+        </div>
+   
+      </div>
+    </div><!-- /.carousel -->
+
+	    <!-- Begin page content -->
+	    
+	    <div class="container">
+		<p class="lead">Concourse is a schemaless and distributed version control database with optimistic availability, serializable transactions and full text search. Concourse provides a more intuitive approach to data management that is easy to deploy, access and scale with minimal tuning while also maintaining the referential integrity and ACID characteristics of traditional database systems.</p>
+		<blockquote><p class="text-muted">Concourse was created for the emerging software landscape where apps are developed rapidly with flexible database needs. Concourse is a database written for programmers&mdash;it works right out the box, no assembly required&mdash;so developers can focus on building great applications without sacrificing performance.</p></blockquote>
+		<h2 style="margin-top: 1em;">Features</h2>
+		<div class="list-group">
+		    <a href="#" class="list-group-item">
+			<h4 class="list-group-item-heading">Simple Data Model</h4>
+			<p class="list-group-item-text">The Concourse data model is lightweight and flexible&mdash;it supports <em>any</em> kind of data at very large scales. All data about a single person, place or thing is held in a <code>record</code>, which is a collection of <code>key/value</code> pairs that are together identified by a unique <code>primary key</code>. A key can map to multiple distinct values, even if those values have different types. And speaking of types, all data within Concourse is dynamically typed&mdash;so yes, it really is that simple.
+			</p>
+		    </a>
+		    <a href="#" class="list-group-item active">
+			<h4 class="list-group-item-heading">Schemaless Design</h4>
+			<p class="list-group-item-text">Since Concourse makes very few assumptions about your data, it integrates seamlessly with your application and never needs a translator (<em>goodbye object-relational impedance mismatch</em>)! You never have to declare any structure up front&mdash;no schema, no tables, no indexes...<em>nothing</em>. Concourse dynamically adapts to your application so that you can build value in new places without having to drag the database along.</p>
+		    </a>
+		    <a href="#" class="list-group-item">
+			<h4 class="list-group-item-heading"><span class="label label-warning" style="margin-right: 1em;"> Coming Soon</span>Scalability and Fault Tolerance</h4>
+			<p class="list-group-item-text">Concourse can be distributed across many different servers on commodity hardware to increase read and write throughput. Distributed data is automatically partitioned and replicated to ensure that the system can withstand arbitrary failure&mdash;all while maintaining ACID strong consistency and optimistic availability. So no need to guess when your database will <em>eventually</em> become accurate&mdash;when distributed Concourse responds to a query, you can trust the results <em>immediately</em>.
+			</p>
+		    </a>
+		    <a href="#" class="list-group-item active ">
+			<h4 class="list-group-item-heading">ACID Transactions</h4>
+			<p class="list-group-item-text">Concourse supports cross-record transactions that are fully ACID compliant: all operations succeed or fail together; writes are visible to all readers only after being successfully committed; serializable isolation is used to prevent dirty reads, non-repeatable reads and phantom reads; and committed transactions are immediately stored to disk so they persist in the event of power loss, crash or error.
+			</p>
+		    </a>
+		    <a href="#" class="list-group-item">
+			<h4 class="list-group-item-heading">Automatic Secondary Indexing</h4>
+			<p class="list-group-item-text">You no longer need to decide what is interesting about your data before you even store it. Concourse automatically indexes everything in both logical and sorted order&mdash;using a buffered storage system to ensure that writes are fast&mdash;so you can quickly fetch and query <em>any</em> key at <em>any</em> time. Concourse fully supports ad-hoc range and predicate queries and automatically caches frequently requested data for optimal performance.</p>
+		    </a>
+		    <a href="#" class="list-group-item active">
+			<h4 class="list-group-item-heading">Version Control</h4>
+			<p class="list-group-item-text">Concourse automatically and efficiently tracks changes to your data. Of course this means that you can easily audit changes and revert to previous states without downtime; but it also means that you have the power to <strong>query and fetch data from the past</strong>. Version control in Concourse makes it possible to build applications that know <em>what was known when</em> and can analyze real-time changes over time.
+			</p>
+		    </a>
+		    <a href="#" class="list-group-item">
+			<h4 class="list-group-item-heading">Full Text Search</h4>
+			<p class="list-group-item-text">Concourse automatically indexes unstructured data so you can perform full text substring searches in real time. Indexes are updated on the fly, so searches are always accurate whenever data is added or removed.</p>
+		    </a>
+		    <a href="#" class="list-group-item active">
+			<h4 class="list-group-item-heading"><span class="label label-warning" style="margin-right: 1em;">Coming Soon</span> Relationships and Graph Queries</h4>
+			<p class="list-group-item-text">Concourse allows you to link records on any key and provides a built in interface to perform graph queries over all the relationships within your data.</p>
+		    </a>
+		</div>
+	    </div>
+	</div>
+
+	<div id="footer">
+	    <div class="container">
+		<p class="text-muted credit">Copyright &copy; 2013 Cinchapi Software Collective. Fork Concourse on <a href="https://github.com/cinchapi/concourse/fork">Github</a>.</p>
+	    </div>
+	</div>
+
+
+	<!-- Bootstrap core JavaScript
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="../js/jquery.min.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Stylish Portfolio Template for Bootstrap</title>
+    <title>Welcome to Cinchapi</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
@@ -23,11 +23,11 @@
     <div id="sidebar-wrapper">
       <ul class="sidebar-nav">
         <a id="menu-close" href="#" class="btn btn-default btn-lg pull-right toggle"><i class="fa fa-times"></i></a>
-        <li class="sidebar-brand"><a href="http://startbootstrap.com">Start Bootstrap</a></li>
+        <li class="sidebar-brand"><a href="http://cinchapi.org">Cinchapi</a></li>
         <li><a href="#top">Home</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#services">Team</a></li>
+        <li><a href="#portfolio">Projects</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
     </div>
@@ -36,8 +36,8 @@
     <!-- Full Page Image Header Area -->
     <div id="top" class="header">
       <div class="vert-text">
-        <h1>Start Bootstrap</h1>
-        <h3><em>We</em> Build Great Templates, <em>You</em> Make Them Better</h3>
+        <h1>This is Cinchapi</h1>
+        <h3>Software to make <em>software</em> better</h3>
         <a href="#about" class="btn btn-default btn-lg">Find Out More</a>
       </div>
     </div>
@@ -48,8 +48,9 @@
       <div class="container">
         <div class="row">
           <div class="col-md-6 col-md-offset-3 text-center">
-            <h2>Subtle Sidebar is the Perfect Template for your Next Portfolio Website Project!</h2>
-            <p class="lead">This template really has it all. It's up to you to customize it to your liking! It features some fresh photography courtesy of <a target="_blank" href="http://join.deathtothestockphoto.com/">Death to the Stock Photo</a>.</p>
+            <h2>Make software. Do great things.</h2>
+            <p class="lead">We support the creation of open source projects 
+		designed to make software development easier.</p>
           </div>
         </div>
       </div>
@@ -61,41 +62,20 @@
       <div class="container">
         <div class="row">
           <div class="col-md-4 col-md-offset-4 text-center">
-            <h2>Our Services</h2>
+            <h2>Our Team</h2>
             <hr>
           </div>
         </div>
         <div class="row">
-          <div class="col-md-2 col-md-offset-2 text-center">
+          <div class="col-md-4 col-md-offset-4 text-center">
             <div class="service-item">
-              <i class="service-icon fa fa-rocket"></i>
-              <h4>Spacecraft Repair</h4>
-              <p>Did your navigation system shut down in the middle of that asteroid field? We can repair any dings and scrapes to your spacecraft!</p>
-            </div>
-          </div>
-          <div class="col-md-2 text-center">
-            <div class="service-item">
-              <i class="service-icon fa fa-magnet"></i>
-              <h4>Problem Solving</h4>
-              <p>Need to know how magnets work? Our problem solving solutions team can help you identify problems and conduct exploratory research.</p>
-            </div>
-          </div>
-          <div class="col-md-2 text-center">
-            <div class="service-item">
-              <i class="service-icon fa fa-shield"></i>
-              <h4>Blacksmithing</h4>
-              <p>Planning a time travel trip to the middle ages? Preserve the space time continuum by blending in with period accurate armor and weapons.</p>
-            </div>
-          </div>
-          <div class="col-md-2 text-center">
-            <div class="service-item">
-              <i class="service-icon fa fa-pencil"></i>
-              <h4>Pencil Sharpening</h4>
-              <p>We've been voted the best pencil sharpening service for 10 consecutive years. If you have a pencil that feels dull, we'll get it sharp!</p>
+              <img src="http://m.c.lnkd.licdn.com/mpr/mpr/shrink_200_200/p/4/000/12f/002/3d76c26.jpg" class="img-circle" style="opacity:0.4"/>
+              
             </div>
           </div>
         </div>
       </div>
+    </div>
     </div>
     <!-- /Services -->
 
@@ -112,37 +92,16 @@
       <div class="container">
         <div class="row">
           <div class="col-md-4 col-md-offset-4 text-center">
-            <h2>Our Work</h2>
+            <h2>Our Projects</h2>
             <hr>
           </div>
         </div>
         <div class="row">
           <div class="col-md-4 col-md-offset-2 text-center">
             <div class="portfolio-item">
-              <a href="#"><img class="img-portfolio img-responsive" src="img/portfolio-1.jpg"></a>
-              <h4>Cityscape</h4>
+              <a href="concourse"><img class="img-portfolio img-responsive" src="concourse/img/slide1.jpg"></a>
+              <h4>Concourse</h4>
             </div>
-          </div>
-          <div class="col-md-4 text-center">
-            <div class="portfolio-item">
-              <a href="#"><img class="img-portfolio img-responsive" src="img/portfolio-2.jpg"></a>
-              <h4>Is That On Fire?</h4>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-4 col-md-offset-2 text-center">
-            <div class="portfolio-item">
-              <a href="#"><img class="img-portfolio img-responsive" src="img/portfolio-3.jpg"></a>
-              <h4>Stop Sign</h4>
-            </div>
-          </div>
-          <div class="col-md-4 text-center">
-            <div class="portfolio-item">
-              <a href="#"><img class="img-portfolio img-responsive" src="img/portfolio-4.jpg"></a>
-              <h4>Narrow Focus</h4>
-            </div>
-          </div>
         </div>
       </div>
     </div>
@@ -153,20 +112,14 @@
       <div class="container">
         <div class="row">
           <div class="col-md-6 col-md-offset-3 text-center">
-            <h3>The buttons below are impossible to resist.</h3>
-            <a href="#" class="btn btn-lg btn-default">Click Me!</a>
-            <a href="#" class="btn btn-lg btn-primary">Look at Me!</a>
+            <h3>Connect with us</h3>
+            <a href="#" class="btn btn-lg btn-default">Wiki</a>
+            <a href="#" class="btn btn-lg btn-primary">Report Issues</a>
           </div>
         </div>
       </div>
     </div>
     <!-- /Call to Action -->
-
-    <!-- Map -->
-    <div id="contact" class="map">
-      <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A"></a></small></iframe>
-    </div>
-    <!-- /Map -->
     
     <!-- Footer -->
     <footer>
@@ -174,15 +127,14 @@
         <div class="row">
           <div class="col-md-6 col-md-offset-3 text-center">
             <ul class="list-inline">
-              <li><i class="fa fa-facebook fa-3x"></i></li>
-              <li><i class="fa fa-twitter fa-3x"></i></li>
-              <li><i class="fa fa-dribbble fa-3x"></i></li>
+              <li><a href="http://twitter.com/cinchapi"><i class="fa fa-facebook fa-3x"></i></a></li>
+              <li><a href="http://twitter.com/cinchapi"><i class="fa fa-twitter fa-3x"></i></a></li>
             </ul>
             <div class="top-scroll">
               <a href="#top"><i class="fa fa-circle-arrow-up scroll fa-4x"></i></a>
             </div>
             <hr>
-            <p>Copyright &copy; Company 2013</p>
+            <p>Copyright &copy; Cinchapi Software Collective 2013</p>
           </div>
         </div>
       </div>
@@ -191,7 +143,7 @@
 
     <!-- Bootstrap core JavaScript -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="js/jquery.js"></script>
+    <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.js"></script>
 
     <!-- Custom JavaScript for the Side Menu and Smooth Scrolling - Put in a custom JavaScript file to clean this up -->
@@ -210,8 +162,8 @@
     <script>
       $(function() {
         $('a[href*=#]:not([href=#])').click(function() {
-          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-            || location.hostname == this.hostname) {
+          if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') 
+            || location.hostname === this.hostname) {
 
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
