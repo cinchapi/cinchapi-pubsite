3a4cb4e94c649339285c1288d04fb611d2ae7968
Jeff Nelson <jtnelson1@gmail.com>
2014-01-18 18:24:37 -0500

    Update downloads to reflect rc2
    Change-Id: If15040323a72b8583fdc238a7a7742cb1cd67bc3

--- concourse/download.php
+++ concourse/download.php
@@ -1,5 +1,5 @@
 <?php
-    $location="https://dl.dropboxusercontent.com/s/p365tftee2emqdc/concourse-server-0.3.0.1141-BETA.bin?dl=1&token_hash=AAFj3TdMtjujxYm0GrZpUzHIRg2IeRmIKiHDufsO7R8u3A";
+    $location="https://dl.dropboxusercontent.com/s/occ0bcd8m0ev6cg/concourse-server-0.3.0.1159-BETA.bin?dl=1&token_hash=AAHvIO2WHokkBdzGhnpkTWM9ydGOeiHnUGKEZRnkx4dd7g";
     header("Location: ".$location);
     die();
 ?>
