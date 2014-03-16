317f80b49671d6408085fdb0c67dfda58c826b8b
Jeff Nelson <jtnelson1@gmail.com>
2014-03-16 09:39:41 -0500

    Updating download links for v0.3.2
    Change-Id: I84cfc3de10544ec82d04edcf6b55be5b66da0d86

--- concourse/download.php
+++ concourse/download.php
@@ -1,5 +1,5 @@
 <?php
-    $location="https://github.com/cinchapi/concourse/releases/download/v0.3.1/concourse-server-0.3.1.1248.bin";
+    $location="https://github.com/cinchapi/concourse/releases/download/v0.3.2/concourse-server-0.3.2.1281.bin";
     header("Location: ".$location);
     die();
 ?>
