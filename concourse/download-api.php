8b0f73e11b0e82715d5ce9cc7747b45afb30665b
Jeff Nelson <jtnelson1@gmail.com>
2014-03-25 10:30:31 -0400

    update download link
    Change-Id: If6a41918a5584affededb269baea778531e281cb

--- concourse/download-api.php
+++ concourse/download-api.php
@@ -1,5 +1,5 @@
 <?php
-    $location="http://search.maven.org/remotecontent?filepath=org/cinchapi/concourse/0.3.1/concourse-0.3.2.jar";
+    $location="http://search.maven.org/remotecontent?filepath=org/cinchapi/concourse/0.3.1/concourse-0.3.3.jar";
     header("Location: ".$location);
     die();
 ?>
--- concourse/download.php
+++ concourse/download.php
@@ -1,5 +1,5 @@
 <?php
-    $location="https://github.com/cinchapi/concourse/releases/download/v0.3.2/concourse-server-0.3.2.1281.bin";
+    $location="https://github.com/cinchapi/concourse/releases/download/v0.3.3/concourse-server-0.3.3.1330.bin";
     header("Location: ".$location);
     die();
 ?>
